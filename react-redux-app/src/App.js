ee63085562d40201816c3d02144778037ed54be2
juancaruizc <juancruizcruz@gmail.com>
2020-12-13 12:30:00 -0700

    styled app

--- react-redux-app/src/App.js
+++ react-redux-app/src/App.js
@@ -1,13 +1,37 @@
 import './App.css';
 import MissionsList from './components/MissionsList'
+import styled from 'styled-components'
+import Logo from './images/SpaceX_Logo_Black.png'
 
 function App() {
   return (
-    <div className="App">
-      <h1>SpaceX Missions</h1>
+    <Container className="App">
+      <Div>
+      <Img src = {Logo} alt = 'spaceX Logo'/>
+      <H1>Missions</H1>
+      </Div>
       <MissionsList />
-    </div>
+    </Container>
   );
 }
+
+const Container = styled.div`
+  background: #F6F6F6;
+`
+
+const Img = styled.img`
+  width:20%;
+  height:5vh;
+`
+
+const Div = styled.div`
+  display:flex;
+  justify-content:center;
+  align-items:center;
+`
+
+const H1 = styled.h1`
+ font-size:1.8rem;
+`
 
 export default App;
--- react-redux-app/src/components/MissionsList.js
+++ react-redux-app/src/components/MissionsList.js
@@ -1,6 +1,32 @@
 import React, {useEffect} from 'react'
 import {connect} from 'react-redux'
 import {fetchMissions} from '../store/actions'
+import styled from 'styled-components'
+
+const Div = styled.div`
+    text-align:left;
+    width:75%;
+    background:white;
+    padding:2%;
+    border-radius:10px;
+    margin-bottom:1%;
+    margin-top:3%;   
+`
+
+const H4 = styled.div`
+    font-size:0.8rem;
+    margin-bottom:2%;
+`
+
+const P = styled.div`
+    font-size:0.7rem;
+`
+
+const Container = styled.div`
+    display:flex;
+    flex-direction:column;
+    align-items:center;
+`
 
 const MissionsList = (props) => {
 
@@ -9,17 +35,17 @@ const MissionsList = (props) => {
     }, [])
 
     return (
-        <div>
+        <Container>
             {props.isLoading ? <p>Loading mission data...</p> : null}
             {props.error ? <p>{props.error}</p>: null}
             {props.missions.map((mission) => (
-                    <div>
-                        <h3>Mission: {mission.mission_name}</h3>
-                        <h4>Manufacturer: {mission.manufacturers}</h4>
-                        <p>{mission.description}</p>
-                    </div>
+                    <Div>
+                        <h3>{mission.mission_name}</h3>
+                        <H4><strong>Manufacturer: </strong>{mission.manufacturers}</H4>
+                        <P>{mission.description}</P>
+                    </Div>
             ))}
-        </div>
+        </Container>
     )
 }
 
